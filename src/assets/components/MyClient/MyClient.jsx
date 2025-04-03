69032cc18d07cfd4384c0d1a9c753ab48a65c1d7
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-03 13:49:50 +0330

    refactor the MyClient component

--- src/assets/components/MyClient/MyClient.jsx
+++ src/assets/components/MyClient/MyClient.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import './MyClient.css'
-
+import React from "react";
+import "./MyClient.css";
+import PopularClientsData from "../PopularClients/PopularClientsData";
 export default function MyClient() {
   return (
     <section className="my-client-container">
-        <p className="my-client-title">My awesome clients</p>
-        <div className="clients-image-container">
-            <img src="../public/image/logo-coinbase.svg" alt="coinbase" className="clients-image" />
-            <img src="../public/image/logo-spotify.svg" alt="spotify" className="clients-image" />
-            <img src="../public/image/logo-pinterest.svg" alt="pinterest" className="clients-image" />
-            <img src="../public/image/logo-google.svg" alt="google" className="clients-image" />
-            <img src="../public/image/logo-amazon.svg" alt="amazon" className="clients-image" />
-            <img src="../public/image/logo-netflix.svg" alt="netflix" className="clients-image" />
-        </div>
+      <p className="my-client-title">My awesome clients</p>
+      <div className="clients-image-container">
+        {PopularClientsData.map((clients) => (
+          <img
+            key={clients.id}
+            src={clients.img}
+            alt={clients.clientName}
+            className="clients-image"
+          />
+        ))}
+      </div>
     </section>
-  )
+  );
 }

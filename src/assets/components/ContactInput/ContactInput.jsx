80251a7d51cc13a493fd9bedde7ff741b016cdfc
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-03 19:57:47 +0330

    refactor the ContactInput component

--- src/assets/components/ContactInput/ContactInput.jsx
+++ src/assets/components/ContactInput/ContactInput.jsx
@@ -1,66 +1,67 @@
-import React from 'react'
-import './ContactInput.css'
+import React, { useState } from "react";
+import "./ContactInput.css";
 
 export default function ContactInput() {
+  // set Hook
+  const [selectedInterest, setSelectedInterest] = useState("");
+  
+  // create handleCheckboxChange func
+  const handleCheckboxChange = (event) => {
+    setSelectedInterest(event.target.value);
+  };
+
   return (
-    <from className='contact-info-input-container'>
-        <div className="name-input-container">
-            <div className="first-name">
-                <input className='name-input' type="text" placeholder='Ayoub'/>
-                <label className='name-label' >Fisrt Name</label>
-            </div>
-            <div className="last-name">
-                <input className='name-input' type="text" placeholder='Akbartabar'/>
-                <label className='name-label' >Last Name</label>
-            </div>
+    <form className="contact-info-input-container">
+      <div className="name-input-container">
+        <div className="first-name">
+          <input className="name-input" type="text" placeholder="Ayoub" />
+          <label className="name-label">First Name</label>
         </div>
-        <div className="email-input-container">
-            <input className='email-input' type="email" placeholder='ayoubakbartabar1887@mail.com'/>
-            <label className='email-label'>Email</label>
+        <div className="last-name">
+          <input className="name-input" type="text" placeholder="Akbartabar" />
+          <label className="name-label">Last Name</label>
         </div>
+      </div>
 
-        <div className="interested-checkbox">
-        
-            <h3 className="interested-checkbox-title">What are you interested on?</h3>
-            
-            <div className="check-boxs-container">
-                <div className="check-box-container">
-                    <label className='checkbox-label'>
-                        <input className='checkbox' type="checkbox" name="Design" />
-                    </label>
-                    <label className='checkbox-title'>Design</label>
-                </div>
-                <div className="check-box-container">
-                    <label className='checkbox-label'>
-                        <input className='checkbox' type="checkbox" name="Development" />
-                    </label>
-                    <label className='checkbox-title'>Development</label>
-                </div>
-                <div className="check-box-container">
-                    <label className='checkbox-label'>
-                        <input className='checkbox' type="checkbox" name="Support" />
-                    </label>
-                    <label className='checkbox-title'>Support</label>
-                </div>
-                <div className="check-box-container">
-                    <label className='checkbox-label'>
-                        <input className='checkbox' type="checkbox" name="Other" />
-                    </label>
-                    <label className='checkbox-title'>Other</label>
-                </div>
+      <div className="email-input-container">
+        <input
+          className="email-input"
+          type="email"
+          placeholder="ayoubakbartabar1887@mail.com"
+        />
+        <label className="email-label">Email</label>
+      </div>
 
+      <div className="interested-checkbox">
+        <h3 className="interested-checkbox-title">
+          What are you interested in?
+        </h3>
+        <div className="check-boxs-container">
+          {["Design", "Development", "Support", "Other"].map((option) => (
+            <div key={option} className="check-box-container">
+              <label className="checkbox-label">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  value={option}
+                  checked={selectedInterest === option}
+                  onChange={handleCheckboxChange}
+                />
+              </label>
+              <label className="checkbox-title">{option}</label>
             </div>
+          ))}
         </div>
+      </div>
 
-        <div className="comment-container">
-            
-            <input className='comment-input' type='text'></input>
-            <label className='comment-label'>Your Message</label>
-            
-        </div>
-        <div className="send-message-btn-container">
-            <button className='send-message-btn'>send message</button>
-        </div>
-    </from>
-  )
+      <div className="comment-container">
+        <input className="comment-input" type="text" />
+        <label className="comment-label">Your Message</label>
+      </div>
+
+      <div className="send-message-btn-container">
+        <button className="send-message-btn">send message</button>
+      </div>
+    </form>
+  );
 }

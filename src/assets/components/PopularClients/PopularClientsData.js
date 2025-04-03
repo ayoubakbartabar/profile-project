80f8e59a8fc55a8f61e0750e49113ada36f8dce1
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-03 20:15:28 +0330

    update PopularClient image root

--- src/assets/components/PopularClients/PopularClientsData.js
+++ src/assets/components/PopularClients/PopularClientsData.js
@@ -2,32 +2,32 @@ const PopularClientsData = [
   {
     id: 1,
     clientName: "coinbaseLogo",
-    img: "../../../../public/logos/logo-coinbase.svg",
+    img: "/logos/logo-coinbase.svg",
   },
   {
     id: 2,
     clientName: "spotifyLogo",
-    img: "../../../../public/logos/logo-spotify.svg",
+    img: "/logos/logo-spotify.svg",
   },
   {
     id: 3,
     clientName: "pinterestLogo",
-    img: "../../../../public/logos/logo-pinterest.svg",
+    img: "/logos/logo-pinterest.svg",
   },
   {
     id: 4,
     clientName: "googleLogo",
-    img: "../../../../public/logos/logo-google.svg",
+    img: "/logos/logo-google.svg",
   },
   {
     id: 5,
     clientName: "amazonLogo",
-    img: "../../../../public/logos/logo-amazon.svg",
+    img: "/logos/logo-amazon.svg",
   },
   {
     id: 6,
     clientName: "netflixLogo",
-    img: "../../../../public/logos/logo-netflix.svg",
+    img: "/logos/logo-netflix.svg",
   },
 ];
 export default PopularClientsData;

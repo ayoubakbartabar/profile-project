7cb9ea8fd26bc3a8103d77ed93cb255b1e311994
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-04 09:27:01 +0330

    update MySkile.css

--- src/assets/components/Projects/Projects.jsx
+++ src/assets/components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import './Projects.css'
 export default function Projects(props) {
   return (
     <div className='project-box'>
-        <img src={props.image} alt={props.title} />
+        <img className='project-image' src={props.image} alt={props.title} />
         <h2 className="project-title">{props.title}</h2>
         <p className="project-pragraph">{props.paragraph}</p>
         <button className='see-detail'>See Details</button>
